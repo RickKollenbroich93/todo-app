b744f671afb592826beddc293c8c080ef06f8a6e
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-07-22 12:17:18 +0200

    Add button component

---
+++ src/components/button/button.stories.js
@@ -0,0 +1,9 @@
+import "./button.css";
+import buttonLogo from "./button-add.html";
+
+export default {
+  title: "Components/Button",
+  parameters: { layout: "fullscreen" },
+};
+
+export const withLogo = () => buttonLogo;
