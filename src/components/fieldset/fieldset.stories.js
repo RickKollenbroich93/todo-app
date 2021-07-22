c547faa1480a8167f7224e7e92437a897beaa25f
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-07-22 15:38:57 +0200

    Add folders

---
+++ src/components/fieldset/fieldset.stories.js
@@ -0,0 +1,9 @@
+import "./fieldset.css";
+import fieldset from "./fieldset.html";
+
+export default {
+  title: "Components/Fieldset",
+  parameters: { layout: "fullscreen" },
+};
+
+export const dog = () => fieldset;
