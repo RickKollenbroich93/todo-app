478fd5d19c757413cf2e7998e53fb65cdd7ddc1e
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-07-23 16:41:36 +0200

    Add all together in taskpage

---
+++ src/components/tasklist/tasklist.stories.js
@@ -0,0 +1,9 @@
+import "./tasklist.css";
+import tasklist from "./tasklist.html";
+
+export default {
+  title: "Components/Tasklist",
+  parameters: { layout: "fullscreen" },
+};
+
+export const taskListbox = () => tasklist;
