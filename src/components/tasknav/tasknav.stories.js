93e0ec7a256bd9a9a34ebf04b484524e62e48ab1
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-07-26 15:23:18 +0200

    Add tasklink component

--- src/components/tasknav/tasknav.stories.js
+++
@@ -1,9 +0,0 @@
-import "./tasknav.css";
-import tasknav from "./tasknav.html";
-
-export default {
-  title: "Components/Tasknav",
-  parameters: { layout: "fullscreen" },
-};
-
-export const taskdate = () => tasknav;
