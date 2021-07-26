32180e8e671d6fc92365797c3dfd01baf72ed97b
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-07-26 15:41:18 +0200

    Add nav-element and Fix Button

---
+++ src/components/taskNav/taskNav.stories.js
@@ -0,0 +1,9 @@
+import "./taskNav.css";
+import tasknav from "./taskNav.html";
+
+export default {
+  title: "Components/TaskNav",
+  parameters: { layout: "fullscreen" },
+};
+
+export const taskNav = () => tasknav;
