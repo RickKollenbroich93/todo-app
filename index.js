dc1e3e209ccc092c7031a46075c7cc383764edcf
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-07-27 16:47:03 +0200

    Add some cool JS stuff

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const mes = "Hallo welt";
 
-alert(mes);
+const menuButton = document.querySelector(".btnburger");
+
+menuButton.onclick = () => sendAlert();
+
+function sendAlert() {
+  alert(mes);
+}
+
+const taskList = document.querySelector(".taskList");
+
+const taskOne = createTaskListItem("Kaffee kochen");
+
+taskList.append(taskOne);
+
+function createTaskListItem(taskName) {
+  const newTaskLabel = document.createElement("label");
+  const newTaskInput = document.createElement("input");
+  const newTaskItem = document.createElement("span");
+
+  newTaskItem.innerText = taskName;
+
+  newTaskLabel.className = "taskItem";
+  newTaskInput.className = "taskItem__checkbox";
+  newTaskInput.type = "checkbox";
+  newTaskInput.setAttribute("name", "tasks");
+  newTaskItem.className = "taskItem__labelText";
+
+  newTaskLabel.append(newTaskInput, newTaskItem);
+
+  return newTaskLabel;
+}
